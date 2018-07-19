7a07c09c2eac3973f65b789e81b27be28374ccfc
EC2 Default User <ec2-user@ip-172-31-27-193.us-east-2.compute.internal>
2018-07-19 09:18:50 +0000

    API取得処理関数化

--- app/Http/Controllers/SearchWordsController.php
+++ app/Http/Controllers/SearchWordsController.php
@@ -11,19 +11,9 @@ class SearchWordsController extends Controller
 {
     public function index(){
         $searchWord = SearchWord::first();
-        $params = [
-            'q'             => $searchWord,
-            'type'          => 'video',
-            'part'          => 'id, snippet',
-            'maxResults'    => 8                        //動画取得件数
-        ];
-        $pageTokens = [];
-        $search = Youtube::paginateResults($params, null);
-        $pageTokens[] = $search['info']['nextPageToken'];
-        $search = Youtube::paginateResults($params, $pageTokens[0]);
-        
-        //   dd($search);
+        $search = $this->ytApi($searchWord);
         // return view('welcome',['videos' => $search,]);
+        dd($search);
         return view('welcome',['videos' => $search, 'keyWords' => $searchWord]);
     
     }
@@ -32,7 +22,23 @@ class SearchWordsController extends Controller
         $name = $req->name;
         $re = DB::table('search_words')->where('word', $name);
         $searchWord = $re->wheres[0]['value'];
+        $search = $this->ytApi($searchWord);
+        return view('welcome',['videos' => $search, 'keyWords' => $searchWord]);
         
+    }
+    
+    public function show(Request $request){
+        $id = $request->id;
+        $video = Youtube::getVideoInfo($id);
+        return view('movie',['video' => $video]);
+    }
+    
+    /*
+    *APIを使ってYouTubeより動画を取得する
+    *引数 $searchWord 検索ワード
+    *return $search   APIで取得した動画（オブジェクト）
+    */
+    private function ytApi($searchWord){
         $params = [
             'q'             => $searchWord,
             'type'          => 'video',
@@ -43,15 +49,7 @@ class SearchWordsController extends Controller
         $search = Youtube::paginateResults($params, null);
         $pageTokens[] = $search['info']['nextPageToken'];
         $search = Youtube::paginateResults($params, $pageTokens[0]);
-        
-        return view('welcome',['videos' => $search, 'keyWords' => $searchWord]);
-        
-    }
-    
-    public function show(Request $request){
-        $id = $request->id;
-        $video = Youtube::getVideoInfo($id);
-        return view('movie',['video' => $video]);
+        return $search;
     }
     
 }
