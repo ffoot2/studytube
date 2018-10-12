b83f9174503fd32a061bf6763e43fb4d047d4df9
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-10-12 11:37:49 +0800

    トップページのレイアウト調整

--- database/migrations/2018_09_08_182147_users_search_words_table.php
+++ database/migrations/2018_09_08_182147_users_search_words_table.php
@@ -22,7 +22,7 @@ class UsersSearchWordsTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('search_word_id')->references('id')->on('search_words')->onDelete('cascade');
             
-            $table->unique([‘user_id’, ‘search_word_id’]);
+            $table->unique(['user_id', 'search_word_id']);
         });
     }
 
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,15 +2,13 @@
 
 @section('content')
 
-<div class="jumbotron p-3 p-md-5 rounded main_visual">
-        <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic">studytube</h1>
-          <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
-          <p class="lead mb-0"><a href="#" class="text-body font-weight-bold">Continue reading...</a></p>
-        </div>
-        
+<div class="jumbotron p-3 p-md-5 rounded main_visual text-white">
+  <div class="col-md-6 px-0">
+    <h1 class="display-4 font-italic">studytube</h1>
+    <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
+    <p class="lead mb-0"><a href="#" class="font-weight-bold">Continue reading...</a></p>
+  </div>
 </div>
-::after
 <div class="row mb-2">
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 shadow-sm h-md-250">
